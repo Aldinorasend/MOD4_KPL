7e52af123e4af555ab14d7f799093bfecf13345c
unknown <aldinorasend@gmail.com>
2024-03-13 07:23:25 +0700

    menambahkan class KodeBuah

--- MOD4_1302223071/Program.cs
+++ MOD4_1302223071/Program.cs
@@ -2,9 +2,27 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public class KodeBuah
+ 
         {
-           
+            public enum namaBuah { Apel, Aprikot, Alpukat, Pisang, Paprika, Blackberry, Ceri, Kelapa, Jagung,Kurma, Durian, Anggur, Melon, Semangka };
+            public string getKodeBuah(namaBuah nama)
+            {
+                string[] kodeBuah = { "A00", "B00", "C00", "D00", "E00", "F00", "H00", "I00", "J00", "K00", "L00", "M00", "N00", "O00" };
+                return kodeBuah[(int)nama];
+                
+
+            }
+            static void Main(string[] args)
+            {
+                KodeBuah kodeBuahObj = new KodeBuah();
+                Console.WriteLine("Nama Buah\t\tKode Buah");
+                foreach(KodeBuah.namaBuah nama in Enum.GetValues(typeof(namaBuah))) { 
+                    String kode1 = kodeBuahObj.getKodeBuah(nama);
+                    Console.WriteLine(nama + "\t\t" + kode1);
+                }
+                
+            }
         }
     }
 }
